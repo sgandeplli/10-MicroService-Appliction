52c7548f1b81637025f6b439b890b91a8f0925b0
sgandeplli <sgandepalli@softility.com>
2025-03-18 10:16:44 +0530

    Update main.tf

--- terra/main.tf
+++ terra/main.tf
@@ -18,3 +18,32 @@ resource "google_container_cluster" "primary" {
 
   remove_default_node_pool = false
 }
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+  default     = "sekhar-452813"  
+}
+
+variable "region" {
+  description = "The region where the resources will be created"
+  type        = string
+  default     = "us-west3"  // Change to a region with sufficient quota
+}
+
+variable "cluster_name" {
+  description = "The name of the Kubernetes cluster"
+  type        = string
+  default     = "my-cluster11" 
+}
+
+variable "node_count" {
+  description = "The number of nodes in the Kubernetes cluster"
+  type        = number
+  default     = 2
+}
+
+variable "node_machine_type" {
+  description = "The type of machine to use for nodes in the Kubernetes cluster"
+  type        = string
+  default     = "e2-standard-4"  // Adjust based on the required machine type
+}
